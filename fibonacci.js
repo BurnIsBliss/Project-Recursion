595c74b7aa90dd40c831bc1923d2100299688cdc
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2024-10-24 14:25:07 +0530

    Implemented the fib series recursively.

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,6 @@
 function fibs(len) {
-	if (len == 1) return [0];
+	if (len <= 0) return [];
+	else if (len == 1) return [0];
 	else if (len == 2) return [0, 1];
 	else {
 		let fibArray = [0, 1];
@@ -13,8 +14,23 @@ function fibs(len) {
 	}
 }
 
-console.log(fibs(1));
-console.log(fibs(2));
-console.log(fibs(8));
-console.log(fibs(15));
-console.log(fibs(7));
+function fibsRec(len, arr = [0, 1]) {
+	if (arr.length >= len) {
+		return arr.slice(0, len);
+	}
+
+	arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+	return fibsRec(len, arr);
+}
+
+// console.log(fibs(1));
+// console.log(fibs(2));
+// console.log(fibs(8));
+// console.log(fibs(15));
+// console.log(fibs(7));
+
+console.log(fibsRec(8));
+// console.log(fibsRec(1));
+// console.log(fibsRec(2));
+console.log(fibsRec(3));
+// console.log(fibsRec(4));
